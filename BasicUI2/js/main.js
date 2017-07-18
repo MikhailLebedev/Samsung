ac7953a5545f0fef53b130ae5c394992799d025d
Mikhail Lebedev <lebedev.m1997@mail.ru>
2017-07-18 12:58:02 +0300

    2

--- BasicUI2/js/main.js
+++ BasicUI2/js/main.js
@@ -1,25 +1,75 @@
-window.onload = function() {
-    // TODO:: Do your initialization job
+$("#two >.ui-content").css("color", "red");
 
-    // add eventListener for tizenhwkey
-    document.addEventListener('tizenhwkey', function(e) {
-        if (e.keyName === "back") {
-            try {
-                tizen.application.getCurrentApplication().exit();
-            } catch (ignore) {}
-        }
-    });
-    
-    
-    var b1 = document.querySelector('#b1');
-    var b2 = document.querySelector('#b2');
-    b1.addEventListener("click", function() {
-        var img = document.querySelector('#msu');
-        img.style.display = (img.style.display === "block") ? "none" : "block";
-    });
-    b2.addEventListener("click", function() {
-        var res = document.querySelector('#res');
 
-        res.innerHTML = (res.innerHTML === "") ? String(screen.width) + ' ' + String(screen.height) : "";
-    });
+var canvas=document.getElementById("draw")
+var x=canvas.getContext("2d");
+x.font = "40pt"
+x.fillStyle ="black"
+x.fillText("Tizen", 15, 40);  
+x.fillStyle ="blue"
+x.fillText("JavaScript", 15, 50);
+x.fillText("canvas", 30, 80); 
+x.fillStyle ="red"
+x.fillText("HTML5", 10, 75);   
+
+
+
+$(document).ready(function(){
+	   $("#draw").draggable({stack:"#drop"});
+	   $("#drop").droppable({
+	      over:function(){
+	         $("#draw").css("border-color","#00ff00");
+	         $("#drop").css("border-color","#00ff00");
+	      },
+	      out:function(){
+		         $("#draw").css("border-color","#ff0000");
+		         $("#drop").css("border-color","#ff0000");
+		      },
+	      drop:function(){
+	         $("#draw").css("visibility","hidden");
+	         $("#drop").css("border-color","#ffffff");
+	         alert("Перемещение завершено успешно.");
+	      }
+	   });
+	});
+
+var startTime;
+var checkTime;
+
+//Initialize function
+var init = function () {
+	// TODO:: Do your initialization job
+	console.log("init() called");
+
+	// add eventListener for tizenhwkey
+	document.addEventListener('tizenhwkey', function(e) {
+		if(e.keyName == "back") {
+			try {
+				tizen.application.getCurrentApplication().exit();
+			} catch (error) {
+				console.error("getCurrentApplication(): " + error.message);
+			}
+		}
+	});
 };
+// window.onload can work without <body onload="">
+window.onload = init;
+
+function startTime() {
+	var today = new Date();
+	var h = today.getHours();
+
+	var m = today.getMinutes();
+	var s = today.getSeconds();
+	m = checkTime(m);
+	s = checkTime(s);
+	document.getElementById('divbutton1').innerHTML="Current time: " + h + ":" + m + ":" + s;
+	var t = setTimeout(startTime, 250);
+}
+
+function checkTime(i) {
+	if (i < 10) {
+		i="0" + i;
+	}
+	return i;
+}
